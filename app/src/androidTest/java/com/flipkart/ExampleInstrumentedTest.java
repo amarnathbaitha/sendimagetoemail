a430754d56b5d81919393e5d960135b648821f89
Amarnath Baitha <amarnathbaitha@gmail.com>
2017-07-18 18:56:54 +0530

    udpated app icon

--- app/src/androidTest/java/com/flipkart/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/flipkart/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.flipkart", appContext.getPackageName());
+        assertEquals("com.catalogue", appContext.getPackageName());
     }
 }
